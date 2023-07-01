2402fc872aaab7c773c8249f8990dc74a6dc2948
xsSplater <xssplater@gmail.com>
2023-07-02 00:44:46 +0300

    Update QuickLookCard_localization.lua

--- QuickLookCard/scripts/mods/QuickLookCard/QuickLookCard_localization.lua
+++ QuickLookCard/scripts/mods/QuickLookCard/QuickLookCard_localization.lua
@@ -72,6 +72,7 @@ return {
 		en = "Curio Perk",
 		["zh-cn"] = "珍品专长",
 		ja = "収集品のパーク",
+		ru = "Перк устройства",
 	},
 	opt_curio_perk_description = {
 		en = "Curios only\nIncludes: rarity indicator with lock color",
