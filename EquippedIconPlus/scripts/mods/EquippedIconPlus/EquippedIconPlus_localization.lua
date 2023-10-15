0bdce06077372508137e9887a874f1d7b9f2ef29
xsSplater <xssplater@gmail.com>
2023-10-15 11:07:40 +0300

    Update RU EquippedIconPlus localization
    Sorry! I need more rest... I even checked in the game.)

--- EquippedIconPlus/scripts/mods/EquippedIconPlus/EquippedIconPlus_localization.lua
+++ EquippedIconPlus/scripts/mods/EquippedIconPlus/EquippedIconPlus_localization.lua
@@ -2,41 +2,51 @@ return {
 	mod_name = {
 		en = "Equipped Icon+",
 		["zh-cn"] = "已装备图标+",
+		ru = "Значок экипированного+",
 	},
 	mod_description = {
 		en = "Show equipped icons for items in other loadouts and customize icons color.",
 		["zh-cn"] = "显示其他配装内物品的已装备图标，并自定义图标颜色。",
+		ru = "Equipped Icon+ - Показывает экипированные значки для предметов в других наборах и позволяет настроить цвет значков.",
 	},
 	opt_active_color = {
 		en = "Equipped icon color (active loadout)",
 		["zh-cn"] = "已装备图标颜色（当前配装）",
+		ru = "Цвет значка (активный набор)",
 	},
 	opt_active_r = {
 		en = "Red",
 		["zh-cn"] = "红",
+		ru = "Красный",
 	},
 	opt_active_g = {
 		en = "Green",
 		["zh-cn"] = "绿",
+		ru = "Зелёный",
 	},
 	opt_active_b = {
 		en = "Blue",
 		["zh-cn"] = "蓝",
+		ru = "Синий",
 	},
 	opt_inactive_color = {
 		en = "Equipped icon color (inactive loadout)",
 		["zh-cn"] = "已装备图标颜色（其他配装）",
+		ru = "Цвет значка (неактивный набор)",
 	},
 	opt_inactive_r = {
 		en = "Red",
 		["zh-cn"] = "红",
+		ru = "Красный",
 	},
 	opt_inactive_g = {
 		en = "Green",
 		["zh-cn"] = "绿",
+		ru = "Зелёный",
 	},
 	opt_inactive_b = {
 		en = "Blue",
 		["zh-cn"] = "蓝",
+		ru = "Синий",
 	},
 }
